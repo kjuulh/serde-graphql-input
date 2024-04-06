9e843bf57d4fb6185619684feef7fcc2595dc4a2
kjuulh <contact@kjuulh.io>
2024-04-06 11:48:27 +0200

    feat: add newtype variant
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- crates/serde-graphql-input/tests/mod.rs
+++ crates/serde-graphql-input/tests/mod.rs
@@ -24,4 +24,18 @@ mod structs {
 
         assert_eq!(r#""something""#, output.as_str())
     }
+
+    #[test]
+    fn can_serialize_newtype_variant() {
+        #[derive(Serialize, Clone, Debug)]
+        enum Input {
+            Something,
+        }
+
+        let input = Input::Something;
+
+        let output = serde_graphql_input::to_string_pretty(&input).unwrap();
+
+        assert_eq!(r#"Something"#, output.as_str())
+    }
 }
